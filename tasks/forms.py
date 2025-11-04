48e3136689198644c9db2939aba7fdbb566d1a94
NuRifat <najimullahrifat@gmail.com>
2025-11-04 13:59:44 +0600

    Django Model Form use

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from tasks.models import Project, Task
 
+# Django Form:
 class TaskForm(forms.Form):
     project = forms.ModelChoiceField(queryset=Project.objects.all(), label="Select Project")
     title = forms.CharField(max_length=250,label="Task Title")
@@ -14,3 +15,23 @@ class TaskForm(forms.Form):
         employees = kwargs.pop("employees",[])
         super().__init__(*args, **kwargs)
         self.fields['assigned_to'].choices = [(emp.id,emp.name) for emp in employees]
+
+# Django Model Form:
+class TaskModelForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        #fields = '__all__' #to bring all the fields from Task
+        fields = ['title','description','due_date','assigned_to']
+        #exclude = [] --> will not show the fields which will included here
+        widgets = {
+            'title' : forms.TextInput(attrs={
+                'class': "border-2 border-gray-300 w-full p-2 rounded-lg focus:border-rose-500 focus:ring-rose-500",
+                'placeholder': "Enter task title"
+            }),
+            'description' : forms.Textarea(attrs={
+                'class': "border-2 border-gray-300 w-full p-2 rounded-lg focus:border-rose-500 focus:ring-rose-500",
+                'placeholder': "Describe the task"
+            }),
+            'due_date': forms.SelectDateWidget,
+            'assigned_to': forms.CheckboxSelectMultiple
+        }
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tasks.forms import TaskForm
+from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import Employee, Task
 
 
@@ -26,31 +26,36 @@ def test(request):
 
 
 def create_task(request):
-    employees = Employee.objects.all()
-    form = TaskForm(employees=employees)
+    #employees = Employee.objects.all()
+    form = TaskModelForm()
 
     if request.method == "POST":
-        form = TaskForm(request.POST, employees=employees)
+        form = TaskModelForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            project = data.get('project')
-            title = data.get('title')
-            description = data.get('description')
-            due_date = data.get('due_date')
-            status = data.get('status')
-            is_completed = data.get('is_completed')
-            assigned_to = data.get('assigned_to')
+            """ For Model Form Data """
+            form.save()
 
-            task = Task.objects.create(
-                project=project, title=title, description=description, due_date=due_date, status=status, is_completed=is_completed
-            )
+            return render(request, 'task_form.html',{"form":form, "message":"task added successfully"})
+            
+            """ For Django Form Data """
+            # data = form.cleaned_data
+            # project = data.get('project')
+            # title = data.get('title')
+            # description = data.get('description')
+            # due_date = data.get('due_date')
+            # status = data.get('status')
+            # is_completed = data.get('is_completed')
+            # assigned_to = data.get('assigned_to')
 
-            #Assign employee to tasks
-            for emp_id in assigned_to:
-                employee = Employee.objects.get(id=emp_id)
-                task.assigned_to.add(employee)
+            # task = Task.objects.create(
+            #     project=project, title=title, description=description, due_date=due_date, status=status, is_completed=is_completed
+            # )
 
-            return HttpResponse("Task added successfully")
+            # #Assign employee to tasks
+            # for emp_id in assigned_to:
+            #     employee = Employee.objects.get(id=emp_id)
+            #     task.assigned_to.add(employee)
+            #return HttpResponse("Task added successfully")
 
     context = {"form":form}
     return render(request, "task_form.html", context)
