f08a6e82361dadc9b0e624d6849ac6565a7d53dd
NuRifat <najimullahrifat@gmail.com>
2025-11-19 15:30:35 +0600

    CRUD-DELETE

--- tasks/models.py
+++ tasks/models.py
@@ -37,7 +37,7 @@ class Task(models.Model):
 
 
 class TaskDetail(models.Model):
-    task = models.OneToOneField(Task,on_delete=models.DO_NOTHING,related_name='details')
+    task = models.OneToOneField(Task,on_delete=models.CASCADE,related_name='details')
     # assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1,choices=[('H','High'),('M','Medium'),('L','Low')],default='L')
     notes = models.TextField(blank=True,null=True)
--- tasks/views.py
+++ tasks/views.py
@@ -139,3 +139,15 @@ def update_task(request,id):
 
     context = {"task_form":task_form, "task_detail_form":task_detail_form}
     return render(request, "task_form.html", context)
+
+
+def delete_task(request,id):
+
+    if request.method == "POST":
+        task = Task.objects.get(id=id)
+        task.delete()
+        messages.success(request,"Task Deleted Successfully")
+        return redirect('manager-dashboard')
+    else:
+        messages.error(request, "Something went wrong")
+        return redirect('manager-dashboard')
