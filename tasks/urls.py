b96acc50a856e6e4fc2db2b051ddf80f839aebe2
NuRifat <najimullahrifat@gmail.com>
2025-11-24 14:36:44 +0600

    Adding Task-Home page

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
+from django.shortcuts import render
 from tasks.views import manager_dashboard, user_dashboard, test, create_task, view_task, update_task,delete_task
 
+def task_home_page(request):
+    return render(request, "dashboard/task-home-page.html")
+
 urlpatterns = [
+    path('home-page', task_home_page, name="task-home"),
     path('manager-dashboard/',manager_dashboard, name="manager-dashboard"),
-    path('user-dashboard/',user_dashboard),
+    path('user-dashboard/',user_dashboard, name="user-dashboard"),
     path('test/',test),
     path('create-task/',create_task, name="create-task"),
-    path('view-task/',view_task),
+    path('view-task/',view_task, name="view-task"),
     path('update-task/<int:id>/',update_task, name="update-task"),
     path('delete-task/<int:id>/',delete_task, name="delete-task"),
 ]
