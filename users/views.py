e71197e6c839f103ee29b9eac47ea8853c961931
NuRifat <najimullahrifat@gmail.com>
2026-01-18 14:56:43 +0600

    Login with custom html form

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from users.forms import RegisterForm, CustomRegistrationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def sign_up(request):
@@ -13,3 +14,18 @@ def sign_up(request):
         else:
             print("Form is not valid")
     return render(request, 'registration/register.html', {"form":form})
+
+def sign_in(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'registration/login.html', {
+                'error': 'Invalid username or password'
+            })
+    return render(request, 'registration/login.html')
