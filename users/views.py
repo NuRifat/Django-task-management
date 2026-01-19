81647a77933f0b0eae6ab92e29cc6169aaf0dc32
NuRifat <najimullahrifat@gmail.com>
2026-01-19 19:49:38 +0600

    Signal file use and Send user activation Email

--- users/views.py
+++ users/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from users.forms import RegisterForm, CustomRegistrationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def sign_up(request):
-    if request.method == 'GET':
-        form = CustomRegistrationForm()
+    form = CustomRegistrationForm()
     if request.method == 'POST':
         form = CustomRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data.get('password1'))
+            user.is_active = False
+            user.save()
+            messages.success(
+                request, 'A Confirmation mail sent. Please check your email')
+            return redirect('sign-in')
+						##### Upto This ######
+						
         else:
             print("Form is not valid")
-    return render(request, 'registration/register.html', {"form":form})
+    return render(request, 'registration/register.html', {"form": form})
 
 def sign_in(request):
     if request.method == 'POST':
