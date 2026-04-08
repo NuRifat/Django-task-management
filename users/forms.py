19b4cb9671dae1bacb142565a83bb228066fd5b0
NuRifat <najimullahrifat@gmail.com>
2026-04-09 04:35:31 +0600

    17.5-7 Custom User Model And Admin Panel Customization

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 import re
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User, Group, Permission
+#from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import Group, Permission
 from tasks.forms import StyledFormMixin
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from users.models import CustomUser
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class RegisterForm(UserCreationForm):
     class Meta:
@@ -100,6 +104,7 @@ class CustomPasswordResetForm(StyledFormMixin, PasswordResetForm):
 class CustomPasswordResetConfirmForm(StyledFormMixin, SetPasswordForm):
     pass
 
+"""
 class EditProfileForm(StyledFormMixin, forms.ModelForm):
     class Meta:
         model = User
@@ -131,4 +136,9 @@ class EditProfileForm(StyledFormMixin, forms.ModelForm):
             user.save()
 
         return user
-
+"""
+        
+class EditProfileForm(StyledFormMixin, forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'bio', 'profile_image']
--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+"""
 class UserProfile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='userprofile', primary_key=True)
@@ -10,3 +12,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} profile"
+"""
+
+
+class CustomUser(AbstractUser):
+    profile_image = models.ImageField(
+        upload_to='profile_images', blank=True, default='profile_images/demo.jpg')
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
